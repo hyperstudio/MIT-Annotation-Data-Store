acd9a25daa67bcd860aff15cfb925ad86226d1e1
Jamie Folsom <jamiefolsom@gmail.com>
2012-06-11 15:05:34 -0400

    Add bootstrap, less middleware, Procfile, .gitignore, getting ready for Heroku Push

--- annotation-fixtures.js
+++ annotation-fixtures.js
@@ -1,5 +1,6 @@
 jQuery.post("/api/annotations", {
-  "updated": "2012-06-08T20:35:59.282554+00:00", 
+  "user": "jamiefolsom",
+  "updated": Date.now(),
   "links": [
     {
       "href": "http://annotateit.org/annotations/CTN4CZ9UTdGboC8jYeXX4g", 
@@ -7,7 +8,17 @@ jQuery.post("/api/annotations", {
       "rel": "alternate"
     }
   ], 
-  "tags": [], 
+  "tags": [ 
+  {
+	"name": "Nantucket"
+  },
+  {
+	"name": "Sailor"
+  },
+  {
+	"name": "Ship"
+  }
+  ], 
   "quote": "streets and avenues\u2014north, east, south, and west. Yet here they all unite. Tell me, does the magnetic virtue of the needles of the compasses of all those ships attract them thither?\n\nOnce more. Say you are in the country; in some high land of lakes. Ta", 
   "ranges": [
     {
@@ -15,20 +26,32 @@ jQuery.post("/api/annotations", {
       "end": "/div/div/p[2]", 
       "startOffset": 2612, 
       "endOffset": 2865
+    }, 
+    {
+      "start": "/div/div/p[2]", 
+      "end": "/div/div/p[2]", 
+      "startOffset": 2610, 
+      "endOffset": 2860
     }
   ], 
-  "user": "jfolsom", 
-  "id": "CTN4CZ9UTdGboC8jYeXX4g", 
   "permissions": {
     "read": [
-      "group:__world__"
+      6, "group:__world__"
     ], 
-    "admin": [], 
-    "update": [], 
-    "delete": []
+    "admin": [
+      6, ""
+	], 
+    "update": [
+      6, ""
+	], 
+    "delete": [
+      6, ""
+	]
   }, 
-  "created": "2012-06-08T20:35:58.928074+00:00", 
+  "created": Date.now(), 
   "uri": "http://blooming-samurai-2296.herokuapp.com/collections/3/documents/3", 
   "text": "asdf", 
-  "consumer": "annotateit"
-}
+  "consumer": "annotationstudio"
+}, function(data, textStatus, jqXHR) { 
+    console.log("Post resposne:"); console.dir(data); console.log(textStatus); console.dir(jqXHR); 
+});
