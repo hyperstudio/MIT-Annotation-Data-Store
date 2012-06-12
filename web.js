5c772ec9dad13e9dd418335990fd0fdbe7291c64
Jamie Folsom <jamiefolsom@gmail.com>
2012-06-12 11:29:36 -0400

    Adding some more CORS headers.

--- web.js
+++ web.js
@@ -9,8 +9,8 @@ var app = express.createServer();
 
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Annotator-Auth-Token, X-Requested-With');
       
     // intercept OPTIONS method
     if ('OPTIONS' == req.method) {
