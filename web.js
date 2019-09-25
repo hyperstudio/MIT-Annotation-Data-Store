9ffd23f864f0cfb7e37491fe22f01ad3297d4b52
Ben Silverman <bsilverm@mit.edu>
2019-09-25 12:38:14 -0400

    Fix deprecated express call

--- web.js
+++ web.js
@@ -24,7 +24,7 @@ var allowCrossDomain = function(req, res, next) {
 
     // intercept OPTIONS method
     if ('OPTIONS' == req.method) {
-        res.send(200);
+        res.sendStatus(200);
     } else {
         next();
     }
